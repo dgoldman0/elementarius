4d7d9987e0eb0e43ea1888dbff9faecd7dd1ad2d
dgoldman0 <45105852+dgoldman0@users.noreply.github.com>
2021-04-26 19:40:29 -0400

    Update setup.sql
    Trying to get code closer to working!

--- setup.sql
+++ setup.sql
@@ -1,9 +1,15 @@
 CREATE DATABASE elementarius_game;
-USE DATABASE elementarius_game;
+USE elementarius_game;
 
-CREATE TABLE Users (id INT NOT NULL auto_increment, username VARCHAR(60) NOT NULL, xp INT UNSIGNED NOT NULL DEFAULT 0, packs SMALLINT UNSIGNED NOT NULL DEFAULT 0, fire INT NOT NULL DEFAULT 0, air INT NOT NULL DEFAULT 0, water INT NOT NULL DEFAULT 0, earth INT NOT NULL DEFAULT 0, light INT NOT NULL DEFAULT 0, darkness INT NOT NULL DEFAULT 0, spirit INT NOT NULL DEFAULT 0);;
+CREATE TABLE Users (id INT NOT NULL auto_increment, username VARCHAR(60) NOT NULL, xp INT UNSIGNED NOT NULL DEFAULT 0,
+  packs SMALLINT UNSIGNED NOT NULL DEFAULT 0, fire INT NOT NULL DEFAULT 0, air INT NOT NULL DEFAULT 0, water INT NOT NULL DEFAULT 0,
+  earth INT NOT NULL DEFAULT 0, light INT NOT NULL DEFAULT 0, darkness INT NOT NULL DEFAULT 0, spirit INT NOT NULL DEFAULT 0);
 
-CREATE PROCEDURE normal(iterations TINYINT)
+CREATE TABLE Stones (id INT UNSIGNED NOT NULL auto_increment, owner_id INT UNSIGNED NOT NULL, mana_type VARCHAR(10) NOT NULL, stone_type VARCHAR(10), energy SMALLINT UNSIGNED NOT NULL);
+
+DELIMITER $$
+
+CREATE PROCEDURE normal(IN iterations TINYINT, OUT result FLOAT)
 READS SQL DATA SQL SECURITY INVOKER
 BEGIN
   DECLARE total FLOAT;
@@ -18,12 +24,10 @@ BEGIN
   END WHILE;
 
   -- total will have a mean of 255n/2 and variance of 255n/12
-  DECLARE value FLOAT
-
   -- Normalize
-  SET value = ((total / 255 / iterations) - 0.5) * sqrt(12)
-  SELECT @value
-END;
+  SET result = ((total / 255 / iterations) - 0.5) * sqrt(12);
+  SELECT @result;
+END;$$
 
 -- Generate a pack and distribute it to the user
 
@@ -32,62 +36,72 @@ READS SQL DATA SQL SECURITY INVOKER
 BEGIN
 
   DECLARE count TINYINT;
-  DECLARE @mana_type TINYINT;
+  DECLARE m_type TINYINT;
+
+  DECLARE mana_rnd FLOAT;
+  DECLARE mana_amt SMALLINT;
+  DECLARE stone_type TINYINT;
+  DECLARE stone_energy TINYINT;
+  DECLARE type_rnd FLOAT;
 
   SET count = 0;
 
   -- I should make sure that a user by user_id actually exists!
 
   REPEAT
-    DECLARE mana_rnd FLOAT;
-    DECLARE mana_amt SMALLINT;
-    DECLARE @stone_type TINYINT;
-    DECLARE stone_energy TINYINT;
 
     SET mana_amt = 0;
-    SET mana_rnd = CALL normal(16);
-
+    CALL normal(16, @a);
+    
+	SELECT @a as mana_rnd;
+    
     -- Make sure that the z-scores are correct for one tail!
     IF mana_rnd < -0.8958 THEN
-      SET @mana_type = 'FIRE';
-    ELSE IF mana_rnd < -0.3309 THEN
-      SET @mana_type = 'AIR';
-    ELSE IF mana_rnd < 0.1397 THEN
-      SET @mana_type = 'WATER';
-    ELSE IF mana_rnd < 0.6456 THEN
-      SET @mana_type = 'EARTH';
-    ELSE IF mana_rnd < 1.0444 THEN
-      SET @mana_type = 'LIGHT';
-    ELSE IF mana_rnd < 1.7862 THEN
-      SET @mana_type = 'DARKNESS';
+      SET m_type = 'FIRE';
+    ELSEIF mana_rnd < -0.3309 THEN
+      SET m_type = 'AIR';
+    ELSEIF mana_rnd < 0.1397 THEN
+      SET m_type = 'WATER';
+    ELSEIF mana_rnd < 0.6456 THEN
+      SET m_type = 'EARTH';
+    ELSEIF mana_rnd < 1.0444 THEN
+      SET m_type = 'LIGHT';
+    ELSEIF mana_rnd < 1.7862 THEN
+      SET m_type = 'DARKNESS';
     ELSE
-      SET @mana_type = 'SPIRIT';
-    END IF
-
-    IF CALL normal(16) > -1.2816 THEN
+      SET m_type = 'SPIRIT';
+    END IF;
+    
+	CALL normal(16, @a);
+    SELECT @a as a;
+    IF a > -1.2816 THEN
       -- Create mana
-      SET mana_amt = EXP(CALL normal(16)) * 100;
-      IF mana_amt < 50 THEN mana_amt = 50 ELSE IF mana_amt > 1000 THEN mana_amt = 1000 END IF;
+      CALL normal(16, @m);
+      SELECT @m as m;
+      
+      SET mana_amt = EXP(m) * 100;
+      IF mana_amt < 50 THEN SET mana_amt = 50; ELSEIF mana_amt > 1000 THEN SET mana_amt = 1000; END IF;
 
       -- Update user's quantity of mana
       IF @mana_type = 'FIRE' THEN UPDATE Users SET fire = fire + mana_amt;
-      ELSE IF @mana_type = 'AIR' THEN UPDATE Users SET fire = air + mana_amt;
-      ELSE IF @mana_type = 'WATER' THEN UPDATE Users SET water = water + mana_amt;
-      ELSE IF @mana_type = 'EARTH' THEN UPDATE Users SET earth = earth + mana_amt;
-      ELSE IF @mana_type = 'LIGHT' THEN UPDATE Users SET light = light + mana_amt;
-      ELSE IF @mana_type = 'DARKNESS' THEN UPDATE Users SET darkness = darkness + mana_amt;
-      ELSE IF @mana_type = 'SPIRIT' THEN UPDATE Users SET spirit = spirit + mana_amt;
-      END IF
+      ELSEIF m_type = 'AIR' THEN UPDATE Users SET fire = air + mana_amt;
+      ELSEIF m_type = 'WATER' THEN UPDATE Users SET water = water + mana_amt;
+      ELSEIF m_type = 'EARTH' THEN UPDATE Users SET earth = earth + mana_amt;
+      ELSEIF m_type = 'LIGHT' THEN UPDATE Users SET light = light + mana_amt;
+      ELSEIF m_type = 'DARKNESS' THEN UPDATE Users SET darkness = darkness + mana_amt;
+      ELSEIF m_type = 'SPIRIT' THEN UPDATE Users SET spirit = spirit + mana_amt;
+      END IF;
     ELSE
       -- Create a mana stone!
 
       -- Determine what kind of stone should be created
-      DECLARE type_rnd FLOAT;
-      SET type_rnd = CALL normal(16);
+      CALL normal(16, @a);
+      SELECT @a as a;
+      SET type_rnd = a;
 
-      IF type_rnd < -1.6449 THEN @stone_type = ` + STONE_INSIGHT + `
-      ELSE IF type_rnd < -0.6745 THEN @stone_type = ` + STONE_CREATION + `
-      ELSE @stone_type = ` + STONE_LIFE + `
+      IF type_rnd < -1.6449 THEN SET stone_type = 'INSIGHT';
+      ELSEIF type_rnd < -0.6745 THEN SET stone_type = 'CREATION';
+      ELSE SET stone_type = 'LIFE';
       END IF
 
       -- Determine what rarity stone should be created
@@ -95,21 +109,21 @@ BEGIN
       SET rarity_rnd = CALL normal(16);
 
       IF rarity_rnd < -3.384196 THEN stone_energy = 100;
-      ELSE IF stone_rnd < -2.764 THEN stone_energy = 700;
-      ELSE IF stone_rnd < -2.047 THEN stone_energy = 4900;
-      ELSE IF stone_rnd < -1.068 THEN stone_energy = 34300;
+      ELSEIF stone_rnd < -2.764 THEN stone_energy = 700;
+      ELSEIF stone_rnd < -2.047 THEN stone_energy = 4900;
+      ELSEIF stone_rnd < -1.068 THEN stone_energy = 34300;
       ELSE stone_energy = 240100;
       END IF
 
       -- Add new mana stone of the created type to the inventory
-      INSERT INTO Stones (owner_id, mana_type, stone_type, energy) VALUES (user_id, @mana_type, @stone_type, stone_energy);
+      INSERT INTO Stones (owner_id, mana_type, stone_type, energy) VALUES (user_id, m_type, @stone_type, stone_energy);
 
-    END IF
-    SET count = count + 1
-  UNTIL count = 4
-  END REPEAT;
+    END IF;
+    
+    SET count = count + 1;
+  UNTIL count = 4 END REPEAT;
 -- I want this statement to return a list of the results as well so that it can be presented to the user.
-END;
+END;$$
 
 CREATE PROCEDURE fuse_stones(IN id_list VARCHAR(1000))
   READS SQL DATA SQL SECURITY INVOKER
@@ -161,5 +175,6 @@ CREATE PROCEDURE fuse_stones(IN id_list VARCHAR(1000))
 
     END REPEAT;
     -- If all goes well, commit data
-    IF error = FALSE THEN COMMIT ELSE ROLLBACK"
-END;
+    IF error = FALSE THEN COMMIT ELSE ROLLBACK;
+END;$$
+DELIMITER;
