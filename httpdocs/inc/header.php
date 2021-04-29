e3ffdb34e290963e0a6e72de5932d03672375fe3
dgoldman0 <45105852+dgoldman0@users.noreply.github.com>
2021-04-29 12:40:34 -0400

    Update header.php

--- httpdocs/inc/header.php
+++ httpdocs/inc/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Arcadium Project</title>
+        <title>Elementarius (Pre-Alpha): Open Mana Pack</title>
 
         <!-- CHARSET AND VIEWPORT METAS AT THE TOP, BELOW TITLE ALWAYS. -->
         <meta charset="utf8">
@@ -13,7 +13,7 @@
 
         <!-- CSS, ICONS AND BOOTSTRAP -->
         <link rel="stylesheet" href="https://bootswatch.com/4/pulse/bootstrap.min.css" />
-        <link rel="stylesheet" href="assets/css/arcadium.css" />
+        <link rel="stylesheet" href="assets/css/elementarius.css" />
         <link rel="icon" type="image/png" href="assets/img/etc.png">
 
         <!-- FontAwesome -->
@@ -40,32 +40,37 @@
       </script>
       <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" width="30px" height="30px" /> Arcadium</a>
+            <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" width="30px" height="30px" /> Elementarius</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarColor02">
-              <ul class="navbar-nav mr-auto">
-                  <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" id="dropdown08" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-toolbox text-white"></i> Resources</a>
-                      <div class="dropdown-menu bg-primary" aria-labelledby="dropdown08">
-                        <a class="dropdown-item text-white" href="/bot.php"><i class="fas fa-toolbox text-white"></i> Twitch Bot</a><
-                        <a class="dropdown-item text-white" href="/resources.php"><i class="fas fa-toolbox text-white"></i> Creator Resources</a><
-                        <hr/>
-                        <a class="dropdown-item text-white" href="https://wrldcoin.io"> World Builder</a>
-                        <a class="dropdown-item text-white" href="https://nolex.org/main/ARCTRX.html?ref=5f27039c6766fa6f943137be"> ARC on NoleX</a>
-                      </div>
-                  </li>
-              </ul>
-
               <ul class="navbar-nav ml-auto">
                   <li class="nav-item"><a class="nav-link" href="https://twitter.com/arcadium0"><i class="fab fa-twitter text-white"></i> Twitter</a></li>
-                  <li class="nav-item"><a class="nav-link" href="/whitepaper.pdf"><i class="fas fa-file-contract text-white"></i> Whitepaper</a></li>
+                  <!--<li class="nav-item"><a class="nav-link" href="/whitepaper.pdf"><i class="fas fa-file-contract text-white"></i> Whitepaper</a></li>-->
                   <!--<li class="nav-item"><a class="nav-link" href="/support_us.php"><i class="fas fa-file-contract text-white"></i> Support Us</a></li>
 
                   <li class="nav-item"><a class="nav-link" href="https://github.com/#" target="_blank"><i class="fab fa-github text-white"></i> GitHub</a></li>
                   <li class="nav-item"><a class="nav-link" href="#" target="_blank"><i class="fas fa-search-dollar text-white"></i> Contract</a></li>-->
+              </ul>
+              <div class="container">
+                <h2>Badges on Buttons</h2>
+                <button type="button" class="btn btn-primary">Primary <span class="badge">7</span></button>
+                <button type="button" class="btn btn-success">Success <span class="badge">3</span></button>
+                <button type="button" class="btn btn-danger">Danger <span class="badge">5</span></button>
+              </div>
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="dropdown08" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-toolbox text-white"></i> [Username]</a>
+                      <div class="dropdown-menu bg-primary" aria-labelledby="dropdown08">
+                        <a class="dropdown-item text-white" href="/bot.php"><i class="fas fa-toolbox text-white"></i> Twitch Bot</a><
+                        <a class="dropdown-item text-white" href="/resources.php"><i class="fas fa-toolbox text-white"></i> Creator Resources</a><
+                        <hr/>
+                        <a class="dropdown-item text-white" href="https://arcadium.fun"> Arcadium</a>
+                        <a class="dropdown-item text-white" href="https://nolex.org/main/ARCTRX.html?ref=5f27039c6766fa6f943137be"> ARC on NoleX</a>
+                      </div>
+                  </li>
               </ul>
           </div>
       </nav>
