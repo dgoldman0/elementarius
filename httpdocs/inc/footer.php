755c5d6f433cf72ddcb36bbdcbb5d9fa0f299ec6
dgoldman0 <45105852+dgoldman0@users.noreply.github.com>
2021-04-29 11:47:20 -0400

    Update footer.php

--- httpdocs/inc/footer.php
+++ httpdocs/inc/footer.php
@@ -1,2 +1,9 @@
+    <!-- Project Dependencies -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/tronweb.js"></script>
+
+    <!-- Dapp Scripts -->
   </body>
 </html>
